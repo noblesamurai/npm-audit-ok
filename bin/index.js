c2aaaa66e99eb6a23b15803ec2d724e8ceef60da
Tim Allen <tim@noblesamurai.com>
2018-12-11 15:15:18 +1100

    Exit with a nice message.

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,10 @@ async function run () {
   const audit = spawn('npm', ['audit', '--json']);
   const output = JSON.parse(await require('get-stream')(audit.stdout));
   const critical = output.metadata.vulnerabilities.critical;
-  if (critical > 0) process.exit(1);
+  if (critical > 0) {
+    console.error('There are critical security vulnerabilities, please resolve these in your package-lock.json.');
+    process.exit(1);
+  }
 }
 
 run();
